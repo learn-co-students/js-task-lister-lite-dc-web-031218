95c20a5106f09cf515b20e58dab1993f51db9553
Max Goodman <max.goodman@flatironschool.com>
2018-05-24 10:12:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-	let store = []
-	let noteId = 0
-
-	class Note{
-		constructor(name, description){
-			this.name = name
-			this.description = description
-			this.id = ++noteId
-			store.push(this)
-		}
-	}
-
-	let notes = store
-	console.log(notes)
 	let submit = document.getElementById('create-task-form')
 	submit.addEventListener('submit', function(e){
 		// fetch = 'post request'
